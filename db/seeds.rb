9d9a36b784057d10b93bf6a9046d259d64890a51
Miriam Fiandanese <mmiy55@gmail.com>
2023-03-30 01:40:51 +0900

    seed goals

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
 require "open-uri"
 
 User.destroy_all
 Goal.destroy_all
 
 def create_admin
-  unless find_by(email: 'admin@me.com') do
+  return if find_by(email: 'admin@me.com')
     # create the admin
     User.create!(
       email: 'admin@me.com',
@@ -24,24 +17,20 @@ def create_admin
     photo_url = "https://this-person-does-not-exist.com#{src}"
     file = URI.open(photo_url)
     user.photo.attach(io: file, filename: 'user.png', content_type: 'image/png')
-  end
-
-
 end
 
-
-new_user = User.find_by(email: 'new@test.com') || User.create!(email: 'new@test.com', password: 'password', first_name: 'Yoshiko', last_name: 'Takagi')
-
-
-goals = Goal.create!([
-  {
-
-  },
-  {
-
-  },
-  {
-
-  }
-]
-)
+Goal.create([{
+    title: "test.#{rand(0..10)}",
+    category: Goal.categories.sample,
+    user: find_by(email: 'admin@me.com')
+},
+{
+    title: "test.#{rand(0..10)}",
+    category: Goal.categories.sample,
+    user: find_by(email: 'admin@me.com')
+},
+{
+    title: "test.#{rand(0..10)}",
+    category: Goal.categories.sample,
+    user: find_by(email: 'admin@me.com')
+}])
