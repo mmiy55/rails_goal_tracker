168e92f4219bd154cde3c8d76d1b0e1c19c4d7f7
Miriam Fiandanese <mmiy55@gmail.com>
2023-03-30 00:56:34 +0900

    add validations to goal

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,3 +1,7 @@
 class Goal < ApplicationRecord
   belongs_to :user
+
+  # validations
+  validates :title, presence: true
+  validates :category, presence: true, inclusion: { in: ['health', 'beauty', 'education', 'work', 'relations', 'eco', 'entertainment', 'other'] }
 end
