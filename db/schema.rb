de673b2123e9d3ae5eeed85e623fd111ac46c528
Miriam Fiandanese <mmiy55@gmail.com>
2023-03-30 00:47:25 +0900

    create goals table

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_29_143754) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_29_154545) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "goals", force: :cascade do |t|
+    t.string "category"
+    t.string "title"
+    t.string "description"
+    t.string "deadline"
+    t.string "measure"
+    t.string "frequency"
+    t.boolean "completed"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_goals_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -26,4 +40,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_29_143754) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "goals", "users"
 end
