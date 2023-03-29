ffda8d549aeb8ead5606f1d525bc95b8435bf680
Miriam Fiandanese <mmiy55@gmail.com>
2023-03-30 01:40:41 +0900

    update categories

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,9 +1,13 @@
 class Goal < ApplicationRecord
   belongs_to :user
 
+  def self.categories
+    ['health', 'beauty', 'education', 'career', 'social', 'spiritual', 'eco', 'recreation', 'financial', 'other']
+  end
+
   # validations
   validates :title, presence: true
-  validates :category, presence: true, inclusion: { in: ['health', 'beauty', 'education', 'work', 'relations', 'eco', 'entertainment', 'other'] }
+  validates :category, presence: true, inclusion: { in: categories }
 
   has_one_attached :photo
 
